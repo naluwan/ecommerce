7ad64f4b24b8ca280bc9ebf426ed73e7997d53b1
naluwan <street.baller06@gmail.com>
2024-03-28 16:22:42 +0800

    feat: update Header, Footer component and update Login pages

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,9 @@ module.exports = {
   printWidth: 100,
   parser: 'typescript',
   semi: false,
+  tabWidth: 2,
   singleQuote: true,
   trailingComma: 'all',
+  bracketSpacing: true,
   arrowParens: 'avoid',
-}
+};
