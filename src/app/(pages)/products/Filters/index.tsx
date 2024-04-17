3726daf248ce644866e35b12128d28b41fb68ada
naluwan <street.baller06@gmail.com>
2024-04-17 17:50:59 +0800

    feat: update Filters component

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,7 +1,69 @@
+'use client'
 import React from 'react'
 
-const Filters = () => {
-  return <div>Filters</div>
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
+import { HR } from '../../../_components/HR'
+import { RadioButton } from '../../../_components/Radio'
+import { useFilter } from '../../../_providers/Filter'
+
+import classes from './index.module.scss'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {
+    if (categoryFilters.includes(categoryId)) {
+      const updatedCategories = categoryFilters.filter(id => id !== categoryId)
+
+      setCategoryFilters(updatedCategories)
+    } else {
+      setCategoryFilters([...categoryFilters, categoryId])
+    }
+  }
+
+  const handleSort = (value: string) => setSort(value)
+
+  return (
+    <div className={classes.filters}>
+      <div>
+        <h6 className={classes.title}>產品類別</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>排序</h6>
+        <div className={classes.categories}>
+          <RadioButton
+            label="最新到最舊"
+            value="-createdAt"
+            isSelected={sort === '-createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+          <RadioButton
+            label="最舊到最新"
+            value="createdAt"
+            isSelected={sort === 'createdAt'}
+            onRadioChange={handleSort}
+            groupName="sort"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Filters
